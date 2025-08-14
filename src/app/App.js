72e3a40b7592e3b0bce2bc9231289646316cd10b
Malay <malaythumbar02@gmail.com>
2025-08-14 09:04:31 +0530

    Switch BrowserRouter to HashRouter for GitHub Pages

--- src/app/App.js
+++ src/app/App.js
@@ -1,6 +1,6 @@
 import React, { lazy } from "react";
 
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import { HelmetMeta } from "./HelmetMeta";
 import { ThemeProvider } from "../components/theme/ThemeProvider";
 import { CssBaseline } from "@material-ui/core";
